8832671b9702d6440647134191c7ca76efab981c
刘赟 <liuyun13@meituan.com>
2017-07-16 22:00:13 -0700

    添加数据库模型

--- InforExtraction/settings.py
+++ InforExtraction/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'InforExtraction.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'RelationExtraction',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '192.168.1.11',
+        'PORT': '3306',
     }
 }
 
--- relations/models.py
+++ relations/models.py
@@ -3,3 +3,29 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class people_list(models.Model):
+	p_id = models.IntegerField(max_length=11)
+	name = models.CharField(max_length=255)
+	introduction = models.TextField()
+	url = models.URLField()
+	photo_url = models.URLField()
+
+class relation_list(models.Field):
+	r_id = models.IntegerField(max_length=11)
+	r_type = models.CharField(max_length=255)
+
+class people_relation(models.Model):
+	n_id = model.IntegerField(max_length=11)
+	p1_id = models.IntegerField(max_length=11)
+	p2_id = models.IntegerField(max_length=11)
+	r_id = models.IntegerField(max_length=11)
+
+class rel_in_sentence(models.Model):
+	s_id = models.IntegerField(max_length=11)
+	sentence = models.CharField(max_length=255)
+	name1 = models.CharField(max_length=255)
+	name2 = models.CharField(max_length=255)
+	r_type = models.CharField(max_length=255)
+	relation = models.CharField(max_length=255)
+	score = models.DecimalField(max_digits=6)
+	url = models.URLField()
