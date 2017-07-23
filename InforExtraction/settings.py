047725ab8ab1b9cba104424f3a5e034749a9da1f
刘赟 <liuyun13@meituan.com>
2017-07-22 23:08:36 -0700

    人物关系搜索异步刷新

--- InforExtraction/settings.py
+++ InforExtraction/settings.py
@@ -72,9 +72,14 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'InforExtraction.wsgi.application'
 
-
+CACHES = {
+     'default': {
+         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+     }
+}
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
