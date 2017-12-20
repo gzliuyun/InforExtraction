c03adfc168afcfa305b386c2a85df3d343ccd35f
monkback <monkback@gmail.com>
2017-12-20 22:15:28 +0800

    add allowed_hosts

--- InforExtraction/settings.py
+++ InforExtraction/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'bj&%bwibwq_)mc_brr%6!l0fd2g+=cfu(@6gi2sm0+&-gzk-zd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
