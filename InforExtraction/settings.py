471b3f8f17c79a9f240a59e223a69ca87ec40efb
monkback <monkback@gmail.com>
2017-12-21 17:41:40 +0800

    add attribute installed_app

--- InforExtraction/settings.py
+++ InforExtraction/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'relations'
+    'relations',
+    'attribute'
 ]
 
 MIDDLEWARE_CLASSES = [
