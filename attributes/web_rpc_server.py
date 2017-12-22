21869e9fe28661da0116dc709cbd1db75cada9b5
monkback <monkback@gmail.com>
2017-12-22 13:24:10 +0800

    remove encode

--- attributes/web_rpc_server.py
+++ attributes/web_rpc_server.py
@@ -48,7 +48,7 @@ def postagger(words):
 
     for pa in result:
         for x in pa[0]:
-            file.write(x.encode('utf-8') + "\t")
+            file.write(x + "\t")
             #file.write(x + "\t")
             file.write(pa[1] + "\n")
     file.close()
