7dfb3ad85ca36a11c759190e9c6ad2f6a483befe
monkback <monkback@gmail.com>
2018-01-03 10:48:04 +0800

    try json.load

--- attributes/web_rpc_server.py
+++ attributes/web_rpc_server.py
@@ -62,7 +62,7 @@ def ner(words,tags):
 	message = json.dumps(sendData).decode().encode('utf8')
 	response = urllib2.urlopen('http://192.168.1.6:10001/', message)
 	data = response.read()
-	jdata = json.loads(data, encoding="utf8")   #jdata即为获取的json数据
+	jdata = json.load(data, encoding="utf8")   #jdata即为获取的json数据
 	netags = jdata['netags']
 
 	return netags
