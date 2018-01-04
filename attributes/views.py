707126fb3a91696ad368c526368264573c2ac2e2
monkback <monkback@gmail.com>
2018-01-04 12:01:19 +0800

    set information page

--- attributes/views.py
+++ attributes/views.py
@@ -111,7 +111,7 @@ def text_upload(request):
 
 def people_search(request):
     request.encoding = 'utf-8'
-    name = request.GET.get('search_name', None).encode('utf8')
+    name = request.GET.get('search_name', None)
     print name
     print "--------"
     url_wiki = "https://zh.wikipedia.org/wiki/" + str(name)
