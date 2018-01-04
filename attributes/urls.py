b589aaaf39c616494002c2a0d97dcf2f49d99c5e
monkback <monkback@gmail.com>
2018-01-04 15:26:36 +0800

    set information page

--- attributes/urls.py
+++ attributes/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns =[
 	url(r'^txt/$', views.attributes_home),
+	url(r'^information/$',views.attributes_network),
 	url(r'^search/$',views.people_search),
 	url(r'^txt/submit/$',views.text_upload),
-	url(r'^information/$',views.attributes_network),
 ]
