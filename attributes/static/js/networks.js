587a8ae6c0e62f8c3fded80ec43ae7e04e8b454a
monkback <monkback@gmail.com>
2018-01-11 20:56:17 +0800

    set font-size

--- attributes/static/js/networks.js
+++ attributes/static/js/networks.js
@@ -67,7 +67,8 @@ function sbt_people(){
                 for(var attr in attributes){
                     var attr_li = document.createElement("li");
                     attr_li.innerHTML = attr + " : " + attributes[attr];
-                    attr_li.style.font-size = "24px";
+                    attr_li.setAttribute("id","attribute_info");
+                    document.getElementById("attribute_info").style.fontWeight = "bold";
                     attributes_div.appendChild(attr_li);
                 }
             }
