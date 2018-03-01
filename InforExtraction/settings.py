b5fb35e5b0f93bf549df8569f20356379bb00a1c
monkback <monkback@gmail.com>
2018-03-01 15:40:13 +0800

    static

--- InforExtraction/settings.py
+++ InforExtraction/settings.py
@@ -131,7 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 # 如果不想用 STATICFILES_DIRS 可以不用，都放在 app 里的 static 中也可以
 
 STATICFILES_DIR = (
-    os.path.join(BASE_DIR,"common_static"),
+    os.path.join(BASE_DIR,"static"),
 )
 
 # 这个是默认设置，Django 默认会在 STATICFILES_DIRS中的文件夹 和 各app下的static文件夹中找文件
