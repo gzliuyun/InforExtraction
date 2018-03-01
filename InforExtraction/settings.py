bb4309089a50ebbe801a2dd3dffd9fba24ac610d
monkback <monkback@gmail.com>
2018-03-01 15:28:58 +0800

    collect static

--- InforExtraction/settings.py
+++ InforExtraction/settings.py
@@ -130,9 +130,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 # 其它 存放静态文件的文件夹，可以用来存放项目中公用的静态文件，里面不能包含 STATIC_ROOT
 # 如果不想用 STATICFILES_DIRS 可以不用，都放在 app 里的 static 中也可以
 
-# STATICFILES_DIR = (
-#     os.path.join(BASE_DIR,"common_static"),
-# )
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR,"common_static"),
+)
 
 # 这个是默认设置，Django 默认会在 STATICFILES_DIRS中的文件夹 和 各app下的static文件夹中找文件
 # 注意有先后顺序，找到了就不再继续找了
