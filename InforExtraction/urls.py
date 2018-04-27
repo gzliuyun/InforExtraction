b1a18ada03596272a5d609cfc1b0189a322398dc
gzliuyun <gz_liuyun@163.com>
2018-04-27 11:55:51 +0800

    修改主页链接域名为knowledge

--- InforExtraction/urls.py
+++ InforExtraction/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from . import views,settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^home/$', views.home),
+    url(r'^knowledge/$', views.home),
     url(r'^login/$', views.login),
     url(r'^relations/', include('relations.urls')),
     url(r'^opinions/', include('opinions.urls')),
